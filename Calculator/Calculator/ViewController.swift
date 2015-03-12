bd0f78a8177cde7086b50d6cecfdf44d87a4db11
Javi Manzano <javi.manzano.oller@gmail.com>
2015-03-12 22:01:52 +0000

    Add operands

--- Calculator/Calculator/ViewController.swift
+++ Calculator/Calculator/ViewController.swift
@@ -24,6 +24,75 @@ class ViewController: UIViewController
             userIsInTheMiddleOfTypingANumber = true
         }
     }
+
+    @IBAction func appendFloatingPoint() {
+        if userIsInTheMiddleOfTypingANumber
+            && display.text!.rangeOfString(".") == nil
+        {
+            display.text = display.text! + "."
+        }
+    }
+
+    @IBAction func appendPi() {
+        if userIsInTheMiddleOfTypingANumber {
+            enter()
+        }
+        display.text = "\(M_PI)"
+        enter()
+    }
+
+
+    @IBAction func operate(sender: UIButton) {
+        let operation = sender.currentTitle!
+
+        if userIsInTheMiddleOfTypingANumber {
+            enter()
+        }
+
+        switch operation {
+        case "×": performOperation { $0 * $1 }
+        case "÷": performOperation { $1 / $0 }
+        case "+": performOperation { $0 + $1 }
+        case "−": performOperation { $1 - $0 }
+        case "√": performOperation { sqrt($0) }
+        case "sin": performOperation { sin($0) }
+        case "cos": performOperation { cos($0) }
+        case "π": performOperation { sqrt($0) }
+        default:
+        break
+        }
+    }
+
+    func performOperation(operation: (Double, Double) -> Double) {
+        if operandStack.count > 1 {
+            displayValue = operation(operandStack.removeLast(), operandStack.removeLast())
+            enter()
+        }
+    }
+
+    func performOperation(operation: (Double) -> Double) {
+        if operandStack.count > 1 {
+            displayValue = operation(operandStack.removeLast())
+            enter()
+        }
+    }
+
+    var operandStack = Array<Double>()
+
+    @IBAction func enter() {
+        userIsInTheMiddleOfTypingANumber = false
+        operandStack.append(displayValue)
+        println("operandStack = \(operandStack)")
+    }
     
+    var displayValue: Double {
+        get {
+            return NSNumberFormatter().numberFromString(display.text!)!.doubleValue
+        }
+        set {
+            display.text = "\(newValue)"
+            userIsInTheMiddleOfTypingANumber = false
+        }
+    }
 }
 
